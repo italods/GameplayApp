40fb861001ff4885f6f6973c9273e2b1005df0df
Italo Pereira <italopereira007@hotmail.com>
2021-07-14 11:25:39 -0300

    SingIn Completa

--- App.tsx
+++ App.tsx
@@ -1,8 +1,36 @@
 import React from 'react';
+import { StatusBar } from 'react-native';
+import { useFonts } from 'expo-font';
+import AppLoading from 'expo-app-loading';
+import { Inter_400Regular, Inter_500Medium  } from '@expo-google-fonts/inter';
+import { Rajdhani_500Medium, Rajdhani_700Bold } from '@expo-google-fonts/rajdhani';
+
+
 import { SingIn } from './src/screens/SingIn';
+import { Background } from './src/components/Background';
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Inter_400Regular,
+    Inter_500Medium,
+    Rajdhani_500Medium,
+    Rajdhani_700Bold
+  });
+
+  //Segura a tela de Splash até carregar as fonts
+  if(!fontsLoaded){
+    return <AppLoading/>
+  }
+
   return (
-    <SingIn />
+    // Tudo esta dentro dos seguintes simbolos "<> </>" se torna uma unica tela por exigencia da linguagem.
+    <Background> 
+      <StatusBar 
+          barStyle="light-content"
+          backgroundColor="transparent"
+          
+        />
+      <SingIn />
+    </Background>
   );
 }
--- src/screens/SingIn/styles.ts
+++ src/screens/SingIn/styles.ts
@@ -7,15 +7,9 @@ export const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: theme.colors.background
+    alignItems: 'center'
   },
-  input:{
-    height: 50,
-    width: 200,
-    borderBottomWidth: 2
-  },
-  image: {
+    image: {
     width: '100%',
     height: 360,
   },
@@ -28,11 +22,15 @@ export const styles = StyleSheet.create({
     textAlign: 'center',
     fontSize: 40,
     marginBottom: 16,
+    fontFamily: theme.fonts.title700,
+    lineHeight: 40
   },
   subtitle: {
     color: theme.colors.heading,
     fontSize: 15,
     textAlign: 'center',
     marginBottom: 64,
+    fontFamily: theme.fonts.title500,
+    lineHeight: 25
   }
 })
