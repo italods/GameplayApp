9759bca79c6fe65f7a8e14558819a3c8ca5993cf
Italo Pereira <italopereira007@hotmail.com>
2021-07-16 00:39:34 -0300

    .

--- App.tsx
+++ App.tsx
@@ -5,11 +5,8 @@ import AppLoading from 'expo-app-loading';
 import { Inter_400Regular, Inter_500Medium  } from '@expo-google-fonts/inter';
 import { Rajdhani_500Medium, Rajdhani_700Bold } from '@expo-google-fonts/rajdhani';
 
-
-
-import { Home } from './src/screens/Home';
-
 import { Background } from './src/components/Background';
+import { Routes } from './src/routes';
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -32,7 +29,7 @@ export default function App() {
           backgroundColor="transparent"
           
         />
-      <Home />
+      <Routes />
     </Background>
   );
 }
--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -4,6 +4,7 @@ import {
   Text,
   Image
 } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
 
 import { ButtonIcon } from '../../components/ButtonIcon';
 
@@ -11,6 +12,12 @@ import IllustrationImg from '../../assets/illustration.png';
 import { styles } from './styles';
 
 export function SignIn(){
+  const navigation = useNavigation();
+
+  function handleSignIn(){
+    navigation.navigate('Home');
+  }
+
   return(
     <View style={styles.container}>
       
@@ -36,7 +43,7 @@ export function SignIn(){
 
       <ButtonIcon 
         title="Entrar com Discord"
-        activeOpacity={0.7} //define a opacidade do botão ao clicar 
+        onPress={handleSignIn}
       />
 
     </View>
