b9a98c64eb68c594b02170a3a3f247751ede9358
Italo Pereira <italopereira007@hotmail.com>
2021-07-21 01:54:45 -0300

    .

--- src/components/Appointment/index.tsx
+++ src/components/Appointment/index.tsx
@@ -2,11 +2,16 @@ import React from 'react';
 import { RectButton, RectButtonProps } from 'react-native-gesture-handler';
 import { Text, View } from 'react-native';
 
+import PlayerSvg from '../../assets/player.svg';
+import CalendarSvg from '../../assets/CalendarSvg.svg';
+
 import { styles } from './styles';
 import { GuildIcon } from '../GuildIcon';
+import { categories } from '../../utils/categories';
+import { theme } from '../../global/styles/theme';
 
 export type GuildProps = {
-
+  owner: true;
 }
 
 export type AppointmentProps = {
@@ -22,10 +27,44 @@ type Props = RectButtonProps & {
 }
 
 export function Appointment({data, ...rest }: Props){
+  const [category] = categories.filter(item => item.id === data.category);
+  const { owner } = data.guild;
+  const { primary, on  } = theme.colors;
+
   return(
     <RectButton {...rest} >
       <View style={styles.container}>
         <GuildIcon />
+
+        <View style={styles.content}>
+          <View style={styles.header}>
+            <Text style={styles.title}>
+              {data.guild.name}
+            </Text>
+            <Text style={styles.category}>
+              { category.title }
+            </Text>
+          </View>
+        </View>
+
+        <View style={styles.footer}>
+          <View style={styles.dateInfo}>
+            <CalendarSvg />
+          </View>
+        </View>
+
+        <View style={styles.playersInfo}>
+          <PlayerSvg fill={ owner ? primary : on }/>
+
+          <Text style={[
+            styles.players,
+            { color: owner ? primary : on }
+          ]}>
+            { owner ? 'Anfitrião' : 'Visitante'}
+          </Text>
+
+        </View>
+
       </View>
     </RectButton>
   )
--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import { View, FlatList, Text } from 'react-native';
+import { Appointment } from '../../components/Appointment';
 
 import { ButtonAdd } from '../../components/ButtonAdd';
 import { CategorySelect } from '../../components/CategorySelect';
@@ -53,7 +54,7 @@ export function Home(){
           data={appointments}
           keyExtractor={item => item.id}
           renderItem={({ item }) => (
-            <Text>{item.guild.name}</Text>
+            <Appointment data={item}/> 
           )}
         />
 
