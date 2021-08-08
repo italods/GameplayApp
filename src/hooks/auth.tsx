083962aede0751b81dab2ddff9fe873ff72f48ca
Italo Pereira <italopereira007@hotmail.com>
2021-08-08 09:52:34 -0300

    .

--- src/hooks/auth.tsx
+++ src/hooks/auth.tsx
@@ -6,6 +6,17 @@ import React,
   ReactNode,
 } from 'react';
 
+import * as AuthSession from 'expo-auth-session';
+
+import { 
+  REDIRECT_URI,
+  SCOPE,
+  RESPONSE_TYPE,
+  CLIENT_ID,
+  CDN_IMAGE
+} from '../configs';
+import { api } from '../service/api';
+
 type User = {
   id: string;
   username: string;
@@ -27,6 +38,21 @@ export const AuthContext = createContext({} as AuthContextData);
 
 function AuthProvider({ children }: AuthProviderProps){
   const [user, setUser] = useState<User>({} as User);
+  const [loading, setLoading] = useState(false);
+
+  function SingIn(){
+    try{
+      setLoading(true);
+
+      const authUrl = `${api.defaults.baseURL}/oauth2/authorize?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=${RESPONSE_TYPE}&scope=${SCOPE}`;
+      
+      AuthSession
+      .startAsync({ authUrl })
+
+    } catch (error) {
+
+    }
+  }
 
   return (
     <AuthContext.Provider value={{
